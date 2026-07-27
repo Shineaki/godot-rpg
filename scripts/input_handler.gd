11aa7a4eb4a8312eed23f3b274652a8a9e3989ff
Shineaki <ubicica>
2026-07-27 13:22:55 +0200

    movement

--- scripts/input_handler.gd
+++ scripts/input_handler.gd
@@ -1,8 +1,9 @@
 extends Node
 
 var direction = Vector2(0, 0)
-var round_counter: int = 0
 var player_node: Node2D = null
+var movement_tween: Tween = null
+var finished_movement = true
 
 func _ready() -> void:
 	player_node = $"../PlayerScene"
@@ -10,14 +11,30 @@ func _ready() -> void:
 func process_user_input():
 	pass
 
-func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_right"):
-		direction.x += 1
-		$"../PlayerScene".attack()
-	if Input.is_action_pressed("ui_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		direction.y += 1
-	if Input.is_action_pressed("ui_up"):
-		direction.y -= 1
-	
+func finished_movement_fn():
+	finished_movement = true
+	movement_tween.kill()
+
+func move_player(dir: Vector2):
+	finished_movement = false
+	player_node.run(dir)
+	movement_tween = create_tween()
+	movement_tween.tween_property(player_node, "position", player_node.position + dir, 0.2)
+	movement_tween.tween_callback(self.finished_movement_fn)
+
+func _process(_delta: float) -> void:
+	if finished_movement:
+		direction = Vector2.ZERO
+		if Input.is_action_pressed("ui_right"):
+			direction = Vector2(16, 0)
+			#$"../PlayerScene".attack()
+		if Input.is_action_pressed("ui_left"):
+			direction = Vector2(-16, 0)
+		if Input.is_action_pressed("ui_down"):
+			direction = Vector2(0, 16)
+		if Input.is_action_pressed("ui_up"):
+			direction = Vector2(0, -16)
+		if direction != Vector2.ZERO:
+			move_player(direction)
+		else:
+			player_node.idle()
--- scripts/player_scene.gd
+++ scripts/player_scene.gd
@@ -1,15 +1,21 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
-	#self.position += 50 * delta * Vector2(1, -1).normalized();
+
+func _process(_delta: float) -> void:
+	pass
 
 func attack() -> void:
 	$AnimationPlayer.play("attack")
+
+func run(dir: Vector2) -> void:
+	$animated_sprite.play("run")
+	if dir.x == -16:
+		$animated_sprite.flip_h = true
+	elif dir.x == 16:
+		$animated_sprite.flip_h = false
+
+func idle() -> void:
+	$animated_sprite.play("idle")
